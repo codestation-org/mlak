f5a3d2d5453a1edfac45e2425235f0f8b492f49e
Marcin Konarski <marcin.konarski@codestation.org>
2017-08-20 00:20:23 +0200

    Add LogisticRegressionSolver and find_solution.

--- mlak/LogisticRegression.py
+++ mlak/LogisticRegression.py
@@ -1,7 +1,9 @@
 from copy import deepcopy
 import LinearAlgebra as la
 import MathTools as mt
+import ModelAnalyzer as ma
 import numpy as np
+import scipy.optimize as optimize
 
 def compute_cost( theta, *args ):
 	X, y, regularizationParam = args
@@ -36,3 +38,31 @@ def predict( X, theta ):
 def predict_one_vs_all( X, all_theta ):
 	return np.argmax( np.dot( X, all_theta.T ), axis = 1 )
 
+class LogisticRegressionSolver( ma.SolverBase ):
+	def __init__( self_, X, y, **kwArgs ):
+		self_._classes, yReverseIndex = np.unique( y, return_inverse = True )
+		la.columnize( yReverseIndex )
+		self_._classCount = len( self_._classes )
+		super().__init__( X, yReverseIndex, **kwArgs )
+
+	def solve( self_, Lambda, **kwArgs ):
+		print( "trying lambda: {}".format( Lambda ), end = "" )
+		n = np.size( self_._dataSet.trainSet.X, 1 )
+		thetas = np.zeros( ( self_._classCount, n ) )
+		for c in range( self_._classCount ):
+			thetas[c] = optimize.fmin_cg(
+				compute_cost,
+				thetas[c], fprime = compute_grad,
+				args = ( self_._dataSet.trainSet.X, ( self_._dataSet.trainSet.y == c ), Lambda ),
+				maxiter = self_._iterations,
+				disp = False
+			)
+		ypCV = predict_one_vs_all( self_._dataSet.crossValidationSet.X, thetas )
+		accuracy = np.mean( 1.0 * ( self_._dataSet.crossValidationSet.y.flatten() == ypCV ) )
+		print( ", accuracy = {}".format( accuracy ) )
+		return ma.Solution( thetas, accuracy, Lambda )
+
+	def test( self_, solution ):
+		yp = predict_one_vs_all( self_._dataSet.testSet.X, solution.theta )
+		accuracy = np.mean( 1.0 * ( self_._dataSet.testSet.y.flatten() == yp ) )
+		return accuracy
--- mlak/ModelAnalyzer.py
+++ mlak/ModelAnalyzer.py
@@ -1,8 +1,10 @@
 import numpy as np
 from collections import namedtuple
+import OptimizationAlgorithms as oa
 
 Observations = namedtuple( "Observations", "X y" )
 DataSet = namedtuple( "DataSet", "trainSet crossValidationSet testSet" )
+Solution = namedtuple( "Solution", "theta accuracy Lambda" )
 
 # Split data to train set, cross validation set and test set.
 # *Fraction tells how much of the data shall go into given set.
@@ -25,3 +27,68 @@ def split_data( X, y, cvFraction, testFraction ):
 	yTest = y[perm[trainSize + cvSize : ]]
 
 	return DataSet( Observations( XTrain, yTrain ), Observations( XCV, yCV ), Observations( XTest, yTest ) )
+
+class SolverBase:
+	def __init__( self_, X, y, **kwArgs ):
+		cvFrequency = kwArgs.get( "cvFrequency", 0.2 )
+		testFrequency = kwArgs.get( "testFrequency", 0.2 )
+
+		self_._iterations = kwArgs.get( "iters", 50 )
+		self_._X = X
+		self_._y = y
+
+		self_._XNormalized, self_._mu, self_._sigma = oa.feature_normalize( self_._X )
+
+		m = np.size( self_._X, 0 )
+		self_._XWithOnes = np.c_[ np.ones( m ), self_._XNormalized ]
+		self_._dataSet = split_data( self_._XWithOnes, self_._y, cvFrequency, testFrequency )
+
+	def solve( self_, Lambda, **kwArgs ):
+		raise Exception( "Method `solve` is not implemented!" )
+
+	def test( self_, solution ):
+		raise Exception( "Method `test` is not implemented!" )
+
+def find_solution( solver, **kwArgs ):
+	lambdaRange = kwArgs.get( "lambdaRange", ( 0, 100 ) )
+	lambdaQuality = kwArgs.get( "lambdaQuality", 1 )
+	lo = lambdaRange[0]
+	hi = lambdaRange[1]
+	Lambda = lo + ( hi - lo ) / 2
+	accuracy = {}
+	get_solution = lambda x : accuracy.get( x ) if x in accuracy else accuracy.setdefault( x, solver.solve( x, **kwArgs ) )
+	solution = None
+	old = []
+	while hi - lo > lambdaQuality:
+		sLo = get_solution( lo )
+		sHyper = get_solution( Lambda )
+		sHi = get_solution( hi )
+		hMax = np.argmin( [sLo.accuracy, sHyper.accuracy, sHi.accuracy] )
+		hMin = np.argmax( [sLo.accuracy, sHyper.accuracy, sHi.accuracy] )
+		if old == [lo, Lambda, hi]: #unstable
+			Lambda, solution = ( lo, sLo ) if hMin == 0 else ( hi, sHi )
+			break
+		old = [lo, Lambda, hi]
+		if hMin == 0:
+			hi = Lambda
+		elif hMin == 2:
+			lo = Lambda
+		else:
+			loT = lo
+			hiT = hi
+			while hiT - loT > 0.01:
+				loT = np.mean( [loT, Lambda] )
+				hiT = np.mean( [Lambda, hiT] )
+				sLoT = get_solution( loT )
+				sHiT = get_solution( hiT )
+				hMin = np.argmax( [sLoT.accuracy, sHyper.accuracy, sHiT.accuracy] )
+				if hMin == 0:
+					hi = Lambda
+					break
+				elif hMin == 2:
+					lo = Lambda
+					break
+		Lambda = np.mean( [lo, hi] )
+		solution = sHyper
+	solution = Solution( solution.theta, solver.test( solution ), solution.Lambda )
+	return solution
