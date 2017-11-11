62d8471ec4f88ae3760d55aa51c9128ba7ec4690
Wojciech Peisert <wpeisert@divante.pl>
2017-11-11 21:36:50 +0100

    model interfaces; in progress...

--- mlak/Interfaces.py
+++ mlak/Interfaces.py
@@ -6,40 +6,124 @@ author: Wojciech Peisert
 Models interfaces
 """
 
+from keras.models import load_model
+import FeatureTools as ft
 
 class NotImplementedException(Exception):
-    def __init__(self, className, methodName):
-        self.value = "class: " + className + " method: " + methodName + " - not implemented!"
-    def __str__(self):
-        return repr(self.value)
+	def __init__(self, className, methodName):
+		self.value = "class: " + className + " method: " + methodName + " - not implemented"
+	def __str__(self):
+		return repr(self.value)
+
+class InterfaceNotImplemented(Exception):
+	def __init__(self, className, interfaceName):
+		self.value = "class: " + className + " is not instance of " + interfaceName
+	def __str__(self):
+		return repr(self.value)
+
+#
+# each model should implement this interface, to be trainable
+#
+class AbstractModelInterface:
+
+	#trains model; can be done incrementally
+	#train(X, y, batch_size=32, epochs=10)
+	def train(self, X, y, **kwArgs):
+		raise NotImplementedException()
+
+	def predict(self, X):
+		raise NotImplementedException()
+
+	def evaluate(self, X, y):
+		raise NotImplementedException()
+
+	def getState(self):
+		raise NotImplementedException()
+
+	def setState(self, stage):
+		raise NotImplementedException()
+
+	def load(self, name, path):
+		raise NotImplementedException()
+
+	def save(self, name, path):
+		raise NotImplementedException()
 
 
-class AbstractModel:
+#
+# This class provides some facilities to AbstractModelInterface
+#
+class AbstractModel(AbstractModelInterface):
 
-    def train(self, X, y, **kwArgs):
-        raise NotImplementedException()
+	def __init__(self, model):
+		self.model = model
+		self.savedStates = {}
 
-    def predict(self, X):
-        raise NotImplementedException()
+	def loadState(self, stateName=""):
+		if not self.savedStates.has_key(stateName):
+			raise Exception("No previously saved state with name " + stateName)
+		self.setState(self.savedStates[stateName])
 
-
-class AbstractSaveable:
-
-    #loads model; name should be unique and valid filename; internal storage structure it up to model
-    def load(path, name):
-        raise NotImplementedException()
-
-    #saves model; name should be unique and valid filename; internal storage structure it up to model
-    def save(path, name):
-        raise NotImplementedException()
+	def saveState(self, stateName=""):
+		self.savedStates[stateName] = self.getState()
 
 
-class AbstractEvaluableModel(AbstractModel):
+"""
+class FeaturedAbstractModel(AbstractModel):
 
-    # it returns valuation of trained model on given data
-    #
-    # this is to be thinked over, if it's a good interface, since valuation depend on model
-    # maybe better solution is to create different interfaces
-    #     depending on type of problem and result meaning
-    def evaluate(self, X, y):
-        raise NotImplementedException()
+	def __init__(self, model, add_features_functions = []):
+		super().__init__(model)
+		self.add_features_functions = add_features_functions
+
+	def train(self, X, y, **kwArgs):
+		super().train(self.__add_features(X), y, **kwArgs)
+
+	def predict(self, X):
+		super().predict(self.__add_features(X))
+
+	def evaluate(self, X, y):
+		super().evaluate(self.__add_features(X), y)
+
+	def __add_features(self, X):
+		return ft.add_features(X, self.add_features_functions)
+"""
+
+#
+# This class implements AbstractModel for Keras (sequantial) model
+#
+class KerasAbstractModel(AbstractModel):
+
+	def train(self, X, y, **kwArgs):
+		self.model.fit(X, y, **kwArgs)
+
+	def predict(self, X):
+		return self.model.predict(X)
+
+	def evaluate(self, X, y):
+		return self.model.evaluate(X, y)
+
+	def getState(self):
+		return self.model.get_weights()
+
+	def setState(self, state):
+		self.model.set_weights(state)
+
+	def load(self, name, path):
+		self.model.save(path + name)
+
+	def save(self, name, path):
+		self.model = load_model(path + name)
+
+
+class KerasModelFactory:
+	#
+	def createModel(**kwArgs):
+		raise Exception("not implemented")
+
+
+
+class ModelTester:
+	def __init__(self, model):
+		if not isinstance(model, AbstractModel):
+			raise InterfaceNotImplemented(obj.__class__.__name__, AbstractModel.__name__)
+		self.model = model
