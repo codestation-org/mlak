27f239fb6b16b8f28ab9c2bcb8b57ffce3655dc5
Marcin Konarski <marcin.konarski@codestation.org>
2018-04-11 21:06:34 +0200

    Add tests for KerasSolver.

--- tests/test_KerasSolver.py
+++ tests/test_KerasSolver.py
@@ -7,7 +7,7 @@ from itertools import product
 import unittest
 import numpy.testing as npt
 
-from NeuralNetwork import *
+from KerasSolver import *
 from ModelAnalyzer import *
 from data_gen import *
 import numpy as np
@@ -15,9 +15,9 @@ import numpy as np
 class TestNeuralNetwork( unittest.TestCase ):
 	def test( self ):
 		X, y = gen_logistic_data()
-		solver = NeuralNetworkSolver()
+		solver = KerasSolver()
 		self.assertEqual( solver.type(), ma.SolverType.CLASSIFIER )
-		solution = solver.train( X, y, nnTopology = "10", Lambda = 1 )
+		solution = solver.train( X, y, nnTopology = "N(10)", Lambda = 1 )
 		self.assertEqual( solution.shaper().class_count(), 3 )
 		cost = solver.verify( solution, X, y )
 		self.assertEqual( cost, 0 )
